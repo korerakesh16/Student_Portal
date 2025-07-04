42a962929d1725dd45e1c14b7100332cc5d4ebb6
korerakesh16 <korerakesh2004@gmail.com>
2025-07-04 13:11:50 +0530

    initial commit

---
+++ src/main/java/com/portal/model/User.java
@@ -0,0 +1,11 @@
+package com.portal.model;
+
+public class User {
+    private int id;
+    private String username;
+    private String email;
+    private String password;
+    private String role;
+
+    // Getters and Setters
+}
