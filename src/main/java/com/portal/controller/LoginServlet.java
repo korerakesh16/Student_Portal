852cbf3bdb164fd58b9a316ddc947b885d809615
korerakesh16 <korerakesh2004@gmail.com>
2025-07-04 13:16:45 +0530

    initial commit

---
+++ src/main/java/com/portal/controller/LoginServlet.java
@@ -0,0 +1,23 @@
+package com.portal.controller;
+
+import com.portal.dao.UserDAO;
+import com.portal.model.User;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+
+        User user = new UserDAO().loginUser(email, password);
+        if (user != null) {
+            HttpSession session = req.getSession();
+            session.setAttribute("user", user);
+            resp.sendRedirect("jsp/home.jsp");
+        } else {
+            resp.getWriter().println("Invalid email or password.");
+        }
+    }
+}
---
+++ src/main/java/com/portal/controller/RegisterServlet.java
@@ -0,0 +1,23 @@
+package com.portal.controller;
+
+import com.portal.dao.UserDAO;
+import com.portal.model.User;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+public class RegisterServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        User user = new User();
+        user.setUsername(req.getParameter("username"));
+        user.setEmail(req.getParameter("email"));
+        user.setPassword(req.getParameter("password"));
+        user.setRole(req.getParameter("role"));
+
+        if (new UserDAO().registerUser(user)) {
+            resp.sendRedirect("jsp/login.jsp");
+        } else {
+            resp.getWriter().println("Registration failed.");
+        }
+    }
+}
