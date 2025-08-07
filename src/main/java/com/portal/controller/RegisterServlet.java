92fb2f3ac1f39cdef9805e84cdef2bd1f8953994
Kore Rakesh <korerakesh2004@gmail.com>
2025-08-07 18:12:30 +0530

    Update RegisterServlet.java

--- src/main/java/com/portal/controller/RegisterServlet.java
+++ src/main/java/com/portal/controller/RegisterServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
 
+
 public class RegisterServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         User user = new User();
